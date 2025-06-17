94519024e23c03045781904a902f61ac0421e526
FERNANDOANGEL202123767 <fernandoangelhernandez62@gmail.com>
2025-06-17 17:45:59 -0600

    Update pelisRoutes.js

--- routes/pelisRoutes.js
+++ routes/pelisRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {pelisController} = require('../Controller/pelisController');
-const { requireAuth } = require('../middleaware/authmiddleaware');
+const { requireAuth } = require('../middleware/authmiddleaware');
 
 router.post('/crear',requireAuth, pelisController.createPelis); 
 router.get('/listar',requireAuth, pelisController.getPelis);
